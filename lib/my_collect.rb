7cd75ac3f31b778fad6225c4fb05d4ce7973a75d
Jeremy Macias <jeremypmacias@gmail.com>
2020-10-26 06:21:28 -0600

    commit tree deac304314da8a25ae01f7ee2211c5b68cec9bac

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+#Take in argument of collection
+#Use while
+#Use yield
+#Return modified collection
 
+
+def my_collect(array)
+    i = 0
+    collection = []
+    while i < array.length
+        collection << yield(array[i])
+        i += 1
+    end
+
+collection
+
+end
 
